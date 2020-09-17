68d505d4dc0568623823de61f6ca755ad697fca7
32465439+platypusmuerte@users.noreply.github.com <32465439+platypusmuerte@users.noreply.github.com>
2020-09-17 17:55:05 -0400

    fixed startup issues

--- main.js
+++ main.js
@@ -9,31 +9,27 @@ const { header } = require("./header");
 const { cleanup } = require("./cleanup");
 const { Utils } = require("./utils/utils");
 const { Database } = require('./db');
-const dataDir = path.join("./", "/data");
-
-const Cryptr = require('cryptr');
-const cryptr = new Cryptr('platyscreditsbot');
-
+let { Listener } = require("./listener/main");
 let utils, listener, db;
 utils = new Utils();
-let { Listener } = require("./listener/main");
 
+if (!fs.existsSync("./data")) {
+	fs.mkdirSync("./data");
+	config.DEBUG && utils.console("Created ./data");
+} else {
+	config.DEBUG && utils.console("./data exists");
+}
+
+const dataDir = path.join("./", "/data");
+const Cryptr = require('cryptr');
+const cryptr = new Cryptr('platyscreditsbot');
 
 header(utils, constants).then(()=>{
 	cleanup(config, path, fs, utils, dataDir).then(() => {
 		config.DEBUG && utils.console(" ");
-		config.DEBUG && utils.console("Looking for ./data");
-
-		if (!fs.existsSync("./data")) {
-			fs.mkdirSync("./data");
-			config.DEBUG && utils.console("Created ./data");
-		} else {
-			config.DEBUG && utils.console("./data exists");
-		}
-	}).then(() => {
-		config.DEBUG && utils.console(" ");
 		config.DEBUG && utils.console("Starting...");
 		config.DEBUG && utils.console(" ");
+
 		db = new Database({ cryptr, dataDir, utils });
 
 		listener = new Listener({ db, utils, exp, dataDir });
